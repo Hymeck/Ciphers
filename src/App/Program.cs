4fadd88dfadffbb78e949b646341a2bbc8b7f4c0
Hymeck <noonimf@gmail.com>
2021-01-28 15:04:32 +0300

    Caesar cipher

--- src/App/Program.cs
+++ src/App/Program.cs
@@ -1,12 +1,28 @@
 ﻿using System;
+using System.Text;
+using Library;
+using static System.Console;
 
 namespace App
 {
     class Program
     {
+        private const string RussianAlphabet =
+            "Аа Бб Вв Гг Дд Ее Ёё Жж Зз Ии Йй Кк Лл Мм Нн Оо Пп Рр Сс Тт Уу Фф Хх Цц Чч Шш Щщ Ъъ Ыы Ьь Ээ Юю Яя";
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            InputEncoding = Encoding.UTF8;
+            OutputEncoding = Encoding.UTF8;
+            
+            var input = args.Length == 0 ? RussianAlphabet : string.Join(' ', args);
+            var key = 3;
+            var encodedInput = Caesar.encipher(input, key);
+            var decodedInput = Caesar.decipher(encodedInput, key);
+            
+            WriteLine($"Входная строка: {input}");
+            WriteLine($"Закодированная  строка по основанию {key}: {encodedInput}");
+            WriteLine($"Раскодированная строка по основанию {key}: {decodedInput}");
+            
         }
     }
 }
--- src/Library/Library.fs
+++ src/Library/Library.fs
@@ -1,5 +1,49 @@
 ﻿namespace Library
 
-module Say =
-    let hello name =
-        printfn "Hello %s" name
+open System
+
+module Caesar =
+
+    let russianUpperLetters = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ"
+
+    let russianLowerLetters = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя"
+    let private charIndex (str: string) (ch: char) = str.IndexOf ch
+
+    let private cipheredIndex letterIndex key letterCount =
+        (letterCount + letterIndex + key) % letterCount
+
+    let private isCharSupported (character: char): bool =
+        charIndex russianLowerLetters character < 0
+        || charIndex russianUpperLetters character < 0
+
+    let private check character =
+        let isLetter = Char.IsLetter character
+
+        let isSupport = isCharSupported character
+
+        isLetter && isSupport
+
+    let private cipher (letter: char) (key: int) (letters: string): char =
+        let letterIndex = letters.IndexOf letter
+
+        let cipheredIndex =
+            cipheredIndex letterIndex key letters.Length
+
+        letters.Chars cipheredIndex
+
+    let private cipherCaller (character: char) (key: int): char =
+        if check character = false then
+            character
+        else
+            let letters =
+                if Char.IsUpper character then russianUpperLetters else russianLowerLetters
+
+            let cipheredLetter = cipher character key letters
+            cipheredLetter
+
+    let encipher input key =
+        // todo: add variants of russian/english/etc.
+        let cipherMapper character = cipherCaller character key
+        String.map cipherMapper input
+
+    let decipher input key = encipher input -key
